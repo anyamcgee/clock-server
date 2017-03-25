b3a56baec0f321d4f640b3459de98c2db71cd3ed
Anya McGee <anyamcgee@gmail.com>
2017-03-25 16:23:11 -0700

    Lol maybe this will deploy it

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@application.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
